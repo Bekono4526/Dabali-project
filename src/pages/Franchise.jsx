08e82956e70f0fbb8228229bcf40c9ca76e78e50
Bekono4526 <bekonorosy0@gmail.com>
2024-07-25 18:29:03 +0200

    franchise-page

--- src/pages/Franchise.jsx
+++ src/pages/Franchise.jsx
@@ -1,69 +1,91 @@
 import React from 'react';
 
 function Franchise() {
+    const sections = [
+        {
+            title: "Pourquoi devenir franchisé Dabali Xpress",
+            text: "Devenir franchise DABALI XPRESS c’est être <strong>la vitrine moderne du tourisme culinaire ivoirien</strong> et un <strong>acteur engagé dans le développement local</strong>.",
+            image: "salut.jpeg",
+            alt: "Franchise Dabali Xpress",
+            buttons: true,
+            className: "md:w-1/3 w-full h-auto object-contain rounded-lg mb-4 md:mb-0 md:mr-8"
+        },
+        {
+            title: "Notre vision",
+            text: "Favoriser le consommer local à travers la promotion des mets ivoiriens et d’autres pays africains est la vision que nous poursuivons. Dabali Xpress œuvre chaque jour à intégrer les habitudes alimentaires de nos contemporains à tous les moments de la journée en leur offrant <strong>toute la richesse et la saveur des recettes de nos grands-mères</strong>. Cadres, Entrepreneurs, Etudiants, Hommes d’affaires, Touristes… sont surs de trouver un produit correspondant à leurs envies tout au long de la journée sur place dans une ambiance chaleureuse ou en livraison à partir du restaurant le plus proche de leur localisation.",
+            image: "miam.jpeg",
+            alt: "Notre vision",
+            buttons: false,
+            className: "md:w-1/3 w-full h-auto object-contain rounded-lg mb-4 md:mb-0 md:mr-8"
+        },
+        {
+            title: "Notre mission",
+            text: "Nos plats, nos salades et nos desserts sont préparés chaque jour sur place dans chacun de nos restaurants. Servir rapidement des plats <strong>sains et savoureux typiquement africains</strong> dans un cadre et environnement sain est la mission accomplie chaque jour dans tous les restaurants Dabali Xpress. Chaque employé est sensibilisé sur cette promesse et travaille dans ce sens. Tous, des cuisiniers aux serveurs en passant par les caissiers s’évertuent chacun à son niveau à faire découvrir et apprécier les produits Dabali Xpress en mettant l’accent sur <strong>la qualité du service</strong>, la disponibilité effective des plats et la bonne tenue du cadre.",
+            images: ["Miem.jpeg", "mium.jpeg"],
+            alt: ["Notre mission 1", "Notre mission 2"],
+            buttons: false,
+            className: "w-full h-auto object-contain rounded-lg mb-4 md:mb-0"
+        },
+        {
+            title: "Notre offre Produit",
+            text: "Dabali Xpress se caractérise par <strong>l’identité et la diversité culinaire africaine</strong>. La marque Dabali Xpress mise sur la production locale pour offrir des mets succulents et originaux au quotidien. Diverses galettes, du pain avec un contenu varié, du café local et continental, des pastels, de la bouillie… disponibles dès les premières heures de la journée sont les composantes du petit déjeuner à Dabali Xpress. Le déjeuner rappelle les jours de fêtes avec un menu comportant au moins 12 plats parmi lesquels <strong>des sauces onctueuses, des soupes, des grillades et des braisés de viandes ou de poissons</strong> servis avec des accompagnements variés allant du foutou, au riz en passant par l’alloco et l’attiéké. Au sein des restaurants Dabali Xpress sont également proposés des <strong>jus de fruits fait à base de fruits naturels pressés traditionnellement</strong> pour conserver toutes les saveurs et les desserts frais pour tous les gouts. Nos chefs cuisiniers revisitent la gastronomie africaine pour concocter à leur façon des recettes <strong>innovantes et savoureuses</strong> qui sont dégustées avec plaisir par la clientèle. <strong>L’innovation est au cœur de l’activité de Dabali Xpress</strong>. Choisir Dabali Xpress, c’est proposer une offre complète, de qualité, originale et accessible.",
+            alt: "Notre offre Produit",
+            buttons: false,
+            className: ""
+        },
+        {
+            title: "Rejoindre DABALI XPRESS, c’est simple !",
+            text: "Vous devez effectuer le parcours décrit étape après étape jusqu’à approbation de notre équipe. <strong>Une panoplie d’opportunités et des perspectives à perte de vue vous attendent avec Dabali Xpress</strong>. Vous vous réjouirez du franc succès de cette activité dans laquelle vous vous êtes investi(s) et de laquelle vous tirerez les meilleurs profits.",
+            image: "schema.jpg",
+            alt: "Rejoindre DABALI XPRESS",
+            buttons: true,
+            className: "md:w-1/3 w-full h-auto object-contain rounded-lg mb-4 md:mb-0 md:mr-8" // Utilisation de object-contain
+        }
+    ];
+
     return (
         <main className="bg-gray-100 p-10 mt-20">
-           <section className="bg-white p-8 rounded-lg shadow-md mb-8 flex">
-                <img src="path_to_image" alt="Franchise Dabali Xpress" className="w-1/2 h-auto object-cover rounded-lg mr-4"/>
-                <div className="flex flex-col justify-center">
-                    <h2 className="text-3xl font-bold mb-4">Pourquoi devenir franchisé Dabali Xpress</h2>
-                    <p className="text-lg text-gray-700 mb-6">
-                        <strong>Devenir franchise DABALI XPRESS c’est être la vitrine moderne du tourisme culinaire ivoirien et un acteur engagé dans le développement local.</strong>
-                    </p>
-                    <div className="flex justify-between">
-                        <button className="bg-red-500 text-white px-4 py-2 rounded-md">Nous contacter</button>
-                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Télécharger la brochure ici</button>
-                    </div>
-                </div>
-            </section>
-
-            <section className="bg-white p-8 rounded-lg shadow-md mb-8 flex flex-col md:flex-row">
-                <img src="path_to_image" alt="Notre vision" className="md:w-1/2 w-full h-auto object-cover rounded-lg mb-4 md:mb-0 md:mr-4"/>
-                <div className="flex flex-col justify-center md:w-1/2">
-                    <h2 className="text-3xl font-bold mb-4">Notre vision</h2>
-                    <p className="text-lg text-gray-700">
-                        <strong>Favoriser le consommer local à travers la promotion des mets ivoiriens et d’autres pays africains</strong> est la vision que nous poursuivons. Dabali Xpress œuvre chaque jour à intégrer les habitudes alimentaires de nos contemporains à tous les moments de la journée en leur offrant dans un laps de temps relativement court toute la richesse et la saveur des recettes de nos grands-mères élaborées à partir de processus minutieux. Cadres, Entrepreneurs, Etudiants, Hommes d’affaires, Touristes… sont surs de trouver un produit correspondant à leurs envies tout au long de la journée sur place dans une ambiance chaleureuse ou en livraison à partir du restaurant le plus proche de leur localisation.
-                    </p>
-                </div>
-            </section>
-
-            <section className="bg-white p-8 rounded-lg shadow-md mb-8 flex flex-col md:flex-row">
-                <img src="path_to_image" alt="Notre mission" className="md:w-1/2 w-full h-auto object-cover rounded-lg mb-4 md:mb-0 md:mr-4"/>
-                <div className="flex flex-col justify-center md:w-1/2">
-                    <h2 className="text-3xl font-bold mb-4">Notre mission</h2>
-                    <p className="text-lg text-gray-700">
-                        Nos plats, nos salades et nos desserts sont préparés chaque jour sur place dans chacun de nos restaurants. <strong>Servir rapidement des plats sains et savoureux typiquement africains dans un cadre et environnement sain est la mission accomplie chaque jour dans tous les restaurants Dabali Xpress.</strong> Chaque employé est sensibilisé sur cette promesse et travaille dans ce sens.
-                        <br/><br/>
-                        Tous, des cuisiniers aux serveurs en passant par les caissiers s’évertuent chacun à son niveau à faire découvrir et apprécier les produits Dabali Xpress en mettant l’accent sur la qualité du service, la disponibilité effective des plats et la bonne tenue du cadre.
-                    </p>
-                </div>
-            </section>
-
-            <section className="bg-white p-8 rounded-lg shadow-md mb-8 flex flex-col md:flex-row">
-                <img src="path_to_image" alt="Notre offre Produit" className="md:w-1/2 w-full h-auto object-cover rounded-lg mb-4 md:mb-0 md:mr-4"/>
-                <div className="flex flex-col justify-center md:w-1/2">
-                    <h2 className="text-3xl font-bold mb-4">Notre offre Produit</h2>
-                    <p className="text-lg text-gray-700">
-                        <strong>Dabali Xpress se caractérise par l’identité et la diversité culinaire africaine.</strong> La marque Dabali Xpress mise sur la production locale pour offrir des mets succulents et originaux au quotidien. Diverses galettes, du pain avec un contenu varié, du café local et continental, des pastels, de la bouillie… disponibles dès les premières heures de la journée sont les composantes du petit déjeuner à Dabali Xpress. Le déjeuner rappelle les jours de fêtes avec un menu comportant au moins 12 plats parmi lesquels des sauces onctueuses, des soupes, des grillades et des braisés de viandes ou de poissons servis avec des accompagnements variés allant du foutou, au riz en passant par l’alloco et l’attiéké. Au sein des restaurants Dabali Xpress sont également proposés des jus de fruits fait à base de fruits naturels pressés traditionnellement pour conserver toutes les saveurs et les desserts frais pour tous les gouts. Nos chefs cuisiniers revisitent la gastronomie africaine pour concocter à leur façon des recettes innovantes et savoureuses qui sont dégustées avec plaisir par la clientèle. <strong>L’innovation est au cœur de l’activité de Dabali Xpress.</strong> Choisir Dabali Xpress, c’est proposer une offre complète, de qualité, originale et accessible.
-                    </p>
-                </div>
-            </section>
-
-            <section className="bg-white p-8 rounded-lg shadow-md mb-8 flex flex-col md:flex-row">
-                <img src="path_to_image" alt="Rejoindre DABALI XPRESS" className="md:w-1/2 w-full h-auto object-cover rounded-lg mb-4 md:mb-0 md:mr-4"/>
-                <div className="flex flex-col justify-center md:w-1/2">
-                    <h2 className="text-3xl font-bold mb-4">Rejoindre DABALI XPRESS, c’est simple !</h2>
-                    <p className="text-lg text-gray-700">
-                        Vous devez effectuer le parcours décrit ci-dessus étape après étape jusqu’à approbation de notre équipe.
-                        <br/><br/>
-                        <strong>Une panoplie d’opportunités et des perspectives à perte de vue vous attendent avec Dabali Xpress.</strong> Vous vous réjouirez du franc succès de cette activité dans laquelle vous vous êtes investi(s) et de laquelle vous tirerez les meilleurs profits.
-                    </p>
-                    <div className="flex justify-between mt-6">
-                        <button className="bg-red-500 text-white px-4 py-2 rounded-md">Nous contacter</button>
-                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Télécharger la brochure ici</button>
-                    </div>
-                </div>
-            </section>
+            {sections.map((section, index) => (
+                <section key={index} className={`bg-white p-8 rounded-lg shadow-md mb-8 flex flex-col md:flex-row ${index % 2 === 1 ? 'md:flex-row-reverse' : ''}`}>
+                    {section.image && (
+                        <img src={section.image} alt={section.alt} className={section.className}/>
+                    )}
+                    {section.images && section.images.length > 0 && (
+                        <div className="relative w-full h-64 mb-4 md:mb-0">
+                            <img src={section.images[0]} alt={section.alt[0]} className="absolute top-0 left-0 w-32 h-32 object-contain rounded-lg"/>
+                            <img src={section.images[1]} alt={section.alt[1]} className="absolute bottom-0 right-0 w-32 h-32 object-contain rounded-lg"/>
+                            <div className="flex flex-col justify-center h-full ml-36 mr-36">
+                                <h2 className="text-3xl font-bold mb-4">{section.title}</h2>
+                                <p className="text-lg text-gray-700" dangerouslySetInnerHTML={{ __html: section.text }}></p>
+                            </div>
+                        </div>
+                    )}
+                    {!section.images && !section.image && (
+                        <div className="flex flex-col justify-center w-full">
+                            <h2 className="text-3xl font-bold mb-4">{section.title}</h2>
+                            <p className="text-lg text-gray-700" dangerouslySetInnerHTML={{ __html: section.text }}></p>
+                            {section.buttons && (
+                                <div className="flex justify-between mt-6">
+                                    <button className="bg-red-500 text-white px-4 py-2 rounded-md">Nous contacter</button>
+                                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Télécharger la brochure ici</button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                    {!section.images && section.image && (
+                        <div className="flex flex-col justify-center md:w-2/3">
+                            <h2 className="text-3xl font-bold mb-4">{section.title}</h2>
+                            <p className="text-lg text-gray-700" dangerouslySetInnerHTML={{ __html: section.text }}></p>
+                            {section.buttons && (
+                                <div className="flex justify-between mt-6">
+                                    <button className="bg-red-500 text-white px-4 py-2 rounded-md">Nous contacter</button>
+                                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Télécharger la brochure ici</button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </section>
+            ))}
 
             <section className="bg-white p-8 rounded-lg shadow-md">
                 <h2 className="text-3xl font-bold mb-4">Articles récents</h2>
@@ -72,7 +94,6 @@ function Franchise() {
                     <li className="text-lg text-gray-700 mb-2"><strong>Dabali Xpress décroche le Prix d’Excellence du meilleur établissement de restauration</strong></li>
                     <li className="text-lg text-gray-700"><strong>Prix du meilleur accueil dans la catégorie Restauration rapide</strong></li>
                 </ul>
-                
             </section>
         </main>
     );
