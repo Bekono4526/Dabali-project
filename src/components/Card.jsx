6edf1c15702b605e566ea67168a4cfe656fb75a2
Bekono4526 <bekonorosy0@gmail.com>
2024-07-31 16:53:12 +0200

    design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useInView } from 'react-intersection-observer';
 
 const Card = ({ imageUrl, headerText, cardText, linkUrl, linkText }) => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
   return (
     <div
-      ref={ref}
-      className={`card relative w-full h-[550px] rounded-lg p-10 flex flex-col justify-end gap-4 text-darker-blue ${
-        inView ? 'card-enter-active' : 'card-enter'
-      }`}
+      className="card relative w-full h-[550px] rounded-lg p-10 flex flex-col justify-end gap-4 text-darker-blue"
       style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
-      <div className="details z-10 flex flex-col gap-4 transform translate-y-full transition-transform duration-500 ease-out">
-        <div className="cardHeader relative uppercase font-bold transition-all duration-500 ease-out">
+      <div className="details z-10 flex flex-col gap-4">
+        <div className="cardHeader relative uppercase font-bold">
           {headerText}
         </div>
         <div className="cardText">
