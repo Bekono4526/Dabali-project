d6bfec20feb547e5ebd32d80049b3141163646e7
Bekono4526 <bekonorosy0@gmail.com>
2024-08-02 15:56:12 +0200

    extensions

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useRef, useState } from "react";
 import { FiShoppingCart, FiX, FiCheckCircle } from "react-icons/fi";
 import HeaderPages from "../components/HeaderPages";
